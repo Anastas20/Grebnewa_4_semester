12f7994628aabbbe82608e26d8d7096ffefcfce8
Anastas20 <106117530+Anastas20@users.noreply.github.com>
2023-06-06 11:29:26 +0300

    Update client.py

--- 5_exercise/client.py
+++ 5_exercise/client.py
@@ -9,11 +9,11 @@ PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+p, g, a = 29, 3, 5  # числа p, g, А могут передаваться по сети, а число а - нет
+A = g ** a % p    # вычисляем число А
+sock.send(pickle.dumps((p, g, A)))  # отправляем числа
 
-B = pickle.loads(sock.recv(1024))
+B = pickle.loads(sock.recv(1024))  # для вычисления числа В получим данные от сервера
 K = B ** a % p
 key = str(K)
 
